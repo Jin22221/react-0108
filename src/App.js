7cbf773797e0dbd734127a685e5f1e8017c2c57d
Jin22221 <117150611+Jin22221@users.noreply.github.com>
2023-01-09 13:56:13 +0800

    學生資料範例(map)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import Counter from './0109/1-counter'
-
+// import Counter from './0109/1-counter'
+// import JSXValue from './0109/2-jsx/2-JSXvalue'
+import Student from './0109/4.map/student'
 function App() {
   return (
     <>
-      <Counter />
+      <Student />
     </>
   )
 }
