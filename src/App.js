60016f156cdccf4f64af35da3a58a06947563d9a
Jin22221 <117150611+Jin22221@users.noreply.github.com>
2023-01-09 11:28:13 +0800

    計數器1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import { useState } from 'react'
+import Counter from './0109/1-counter'
 
 function App() {
-  //宣告狀態用的語法
-  const [total, setTotal] = useState(0)
-
   return (
     <>
-      <h1
-        onClick={() => {
-          //不可使用total++，因為相當於total += 1
-          setTotal(total + 1)
-        }}
-      >
-        {total}
-      </h1>
+      <Counter />
     </>
   )
 }
