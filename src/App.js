581184a4c85084872c24e384ee803d31b828f75b
Jin22221 <117150611+Jin22221@users.noreply.github.com>
2023-01-10 18:22:54 +0800

    count +

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // import Counter from './0109/1-counter'
 // import JSXValue from './0109/2-jsx/2-JSXvalue'
 // import Parent from './0110/3-prop-父母子女元件/Parent'
-import Exam2 from './0110/5-0.練習題-物件陣列處理練習/Exam2'
+import ShoppingCart from './0110/6-1.練習題-小型購物車/ShoppingCart'
 function App() {
   return (
     <>
-      <Exam2 />
+      <ShoppingCart />
     </>
   )
 }
