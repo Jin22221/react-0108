a0467cfecc108b91754ac14fc264da6105bac8c4
Jin22221 <117150611+Jin22221@users.noreply.github.com>
2023-01-10 13:57:41 +0800

    4.練習題

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import Counter from './0109/1-counter'
 // import JSXValue from './0109/2-jsx/2-JSXvalue'
-import Parent from './0110/3-prop-父母子女元件/Parent'
+// import Parent from './0110/3-prop-父母子女元件/Parent'
+import Menu from './0110/4.練習題-選單樣式套用/Menu'
 function App() {
   return (
     <>
-      <Parent />
+      <Menu />
     </>
   )
 }
