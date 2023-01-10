0fc163e2eff43649fa3ccb3340bfafb5bebfad3f
Jin22221 <117150611+Jin22221@users.noreply.github.com>
2023-01-10 13:59:47 +0800

    以點選到的選單項目文字作為state

--- src/0110/4.練習題-選單樣式套用/Menu.js
+++ src/0110/4.練習題-選單樣式套用/Menu.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './Menu.css'
 
 function Menu() {
+  //以點選到的選單項目文字作為state
   const [activeText, setActiveText] = useState('')
 
   const menuItems = ['首頁', '關於我們', '產品']
