00f965abd9423d24b34e191885f1c61478c71cb7
Jin22221 <117150611+Jin22221@users.noreply.github.com>
2023-01-14 11:43:10 +0800

    共用表單輸入

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // import Counter from './0109/1-counter'
 // import JSXValue from './0109/2-jsx/2-JSXvalue'
 // import Parent from './0110/3-prop-父母子女元件/Parent'
-import Idform from './0114/idform'
-import RefsForm from './0114/refsForm'
+// import Idform from './0114/idform'
+// import RefsForm from './0114/refsForm'
+import MultipleInput from './0114/共用表單輸入'
 function App() {
   return (
     <>
-      <Idform />
-      <RefsForm />
+      <MultipleInput />
     </>
   )
 }
