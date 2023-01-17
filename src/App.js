8853a84f8a17b121d29f1bf9ccc337309343ee3f
Jin22221 <117150611+Jin22221@users.noreply.github.com>
2023-01-17 11:18:15 +0800

    拆解元件

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@
 // import Parent from './0110/3-prop-父母子女元件/Parent'
 // import Idform from './0114/idform'
 // import RefsForm from './0114/refsForm'
-import MultipleInput from './0114/HTML5ValidForm'
+import TodoApp from './0117/TodoApp'
 function App() {
   return (
     <>
-      <MultipleInput />
+      <TodoApp />
     </>
   )
 }
