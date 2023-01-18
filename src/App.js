887e3abcaaa64bb4ef298784dc7298e3699ff63c
Jin22221 <117150611+Jin22221@users.noreply.github.com>
2023-01-18 14:52:29 +0800

    3-3.react-bootstrap範例

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 // import Parent from './0110/3-prop-父母子女元件/Parent'
 // import Idform from './0114/idform'
 // import RefsForm from './0114/refsForm'
-import User from './0118/2-4載入指示器/User'
+import User from './0118/3-3.react-bootstrap範例/0118/4.react-bs/ReactBS5'
 function App() {
   return (
     <>
