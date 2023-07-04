985e11ac22990ab13d1c2342ee5fee056877afa5
Jin22221 <117150611+Jin22221@users.noreply.github.com>
2023-07-04 23:38:13 +0800

    array of obj of various operations

--- src/0110/5-0.練習題-物件陣列處理練習/Exam2.js
+++ src/0110/5-0.練習題-物件陣列處理練習/Exam2.js
@@ -23,6 +23,7 @@ const objArray = [
 function Exam2() {
   // 與呈現有關必需先成為state
   const [data, setData] = useState(objArray)
+  // console.log(data, 'data')
 
   return (
     <>
@@ -51,24 +52,23 @@ function Exam2() {
       <h2>操作</h2>
       <p>
         <strong>
-          注意: 請在任一操作前先重新整理網頁 ，或是對重覆id值進行加入時的限制。
+          注意: 請在任一操作前先重新整理網頁，或是對重覆id值進行加入時的限制。
         </strong>
         有些操作是key值會對應id的關係，會產生key值重覆問題，造成不預期的結果。實務上務必要考慮key不可以重覆問題。
       </p>
-
+      {/* ================================================================= */}
       <button
+        // 1. 從目前的狀態拷貝(深拷貝/完全拷貝)出一個新的變數值(陣列/物件)
+        // 2. 在新的變數值(陣列/物件)上作處理
+        // 3. 設定回原本的狀態中
         onClick={() => {
           // 先寫出要新增的物件值
           const newObj = { id: 99, text: 'xxx' }
 
-          // 1. 從目前的狀態拷貝出一個新的變數值(陣列/物件)
-          // 2. 在新的變數值(陣列/物件)上作處理
-          // 3. 設定回原本的狀態中
-
           //1 //2
-          //簡單的申拷貝語法
-          const cloneData = JSON.parse(JSON.stringify(data))
-          console.log(cloneData, 'clonedata')
+          //簡單的深拷貝語法
+          // const cloneData = JSON.parse(JSON.stringify(data))
+          // console.log(cloneData, 'clonedata')
 
           const newData = [newObj, ...data]
 
@@ -79,6 +79,7 @@ function Exam2() {
         列表最前面，新增一個物件值id為99與文字為xxx的物件
       </button>
       <br />
+      {/* ================================================================= */}
       <button
         onClick={() => {
           const newObj = { id: 88, text: 'yyy' }
@@ -93,14 +94,45 @@ function Exam2() {
         列表最後面，新增一個物件值id為88與文字為yyy的物件
       </button>
       <br />
-      <button onClick={() => {}}>
+      {/* ================================================================= */}
+      <button
+        onClick={() => {
+          // id(數字遞增)
+          const ids = data.map((v) => v.id)
+          const maxId = Math.max(...ids)
+          const newId = maxId + 1
+
+          const newObj = {
+            id: newId,
+            text: 'xxx',
+          }
+
+          const newData = [newObj, ...data]
+
+          setData(newData)
+        }}
+      >
         列表最前面，新增一個文字為xxx的物件(id不能與其它資料重覆)
       </button>
       <br />
-      <button onClick={() => {}}>
+      {/* ================================================================= */}
+      <button
+        onClick={() => {
+          const newObj = {
+            // 或 +new Date()
+            id: Number(new Date()),
+            text: 'yyy',
+          }
+
+          const newData = [...data, newObj]
+
+          setData(newData)
+        }}
+      >
         列表最後面，新增一個文字為yyy的物件(id不能與其它資料重覆)
       </button>
       <br />
+      {/* ================================================================= */}
       <button
         onClick={() => {
           //
@@ -114,50 +146,52 @@ function Exam2() {
         尋找(過濾)只呈現所有文字中有包含a英文字母的資料
       </button>
       <br />
+      {/* ================================================================= */}
       <button
         onClick={() => {
-          //刪除相當於建立一個新陣列 中沒有文字b的物件
-          //不是文字b的物件才能到新陣列
+          // 刪除相當於建立一個新陣列中沒有文字b的物件
+          // 不是文字b的物件才能到新陣列
           const newData = data.filter((v, i) => {
             return v.text !== 'b'
           })
+
           setData(newData)
         }}
       >
         刪除文字為b的物件資料
       </button>
       <br />
+      {/* ================================================================= */}
       <button
         onClick={() => {
           const newData = data.filter((v, i) => {
             return v.id !== 4
           })
+
           setData(newData)
         }}
       >
         刪除id為4的物件資料
       </button>
       <br />
+      {/* ================================================================= */}
       <button
         onClick={() => {
-          //尋找id=2的索引值為何
-          const index = data.findIndex((v, i) => {
-            return v.id === 2
-          })
+          // 尋找 id = 2 的索引值為何
+          const index = data.findIndex((v, i) => v.id === 2)
           console.log(index)
           console.log(data.slice(0, index + 1), data.slice(index + 1))
-          //if找到=>切開,插入要加入的物件,合併
+          // if 找到=>切開,插入要加入的物件,合併
           if (index !== -1) {
-            //有找到id 2
-            //slice
-            //插入
-            const newObj = { id: 5, text: 'bbb' }
+            const newObj = {
+              id: 5,
+              text: 'bbb',
+            }
             const newData = [
               ...data.slice(0, index + 1),
               newObj,
               ...data.slice(index + 1),
             ]
-
             setData(newData)
           }
         }}
@@ -165,25 +199,37 @@ function Exam2() {
         在id為2後面插入id為5與文字為bbb的物件
       </button>
       <br />
+      {/* ================================================================= */}
       <button
         onClick={() => {
-          // 1. 從目前的狀態拷貝出一個新的變數值(陣列/物件)
+          // 1. 從目前的狀態拷貝(深拷貝/完全拷貝)出一個新的變數值(陣列/物件)
+          // 深拷貝 // newData目前還不是狀態，要等到設定回去之後
+          // 可以多拷貝一層 =>陣列中的物件的語法
+          // const newData = data.map((v, i) => {
+          //   // 把每個物件值做淺拷貝
+          //   return { ...v }
+          // })
+          // console.log(newData)
+          // // 2. 在新的變數值(陣列/物件)上作處理
+          // const index = newData.findIndex((v, i) => {
+          //   return v.id === 3
+          // })
+          // console.log(index)
+          // if (index !== -1) {
+          //   newData[index]['text'] = 'cccc'
+          // }
+          // // 3. 設定回原本的狀態中
+          // setData(newData)
+
+          // ------精簡的寫法
           const newData = data.map((v, i) => {
-            //把每個物件值作淺拷貝
+            if (v.id === 3) return { ...v, text: 'cccc' }
+
+            // id 不是 3 的話，把原本的資料展開，加到新的陣列去
             return { ...v }
           })
-          console.log(newData, 'newData')
-          // 2. 在新的變數值(陣列/物件)上作處理
-          const index = newData.findIndex((v, i) => {
-            return v.id === 3
-          })
-          console.log(index)
 
-          if (index !== -1) {
-            newData[index].text = 'cccc'
-            // 3. 設定回原本的狀態中
-            setData(newData)
-          }
+          setData(newData)
         }}
       >
         取代id為3的文字為cccc
--- src/App.js
+++ src/App.js
@@ -8,9 +8,13 @@
 // import Gearing from './0109/8.useEffect連動/Gearing'
 // import Parent from './0110/1.props解構2/Parent'
 // import Parent from './0110/3-0.prop-父母子女元件C到P'
-import Parent from './0110/3-1.prop-父母子女元件C到C/Parent'
+// import Parent from './0110/3-1.prop-父母子女元件C到C/Parent'
+// import Menu from './0110/4.練習題-選單樣式套用/Menu'
+
+import Exam2 from './0110/5-0.練習題-物件陣列處理練習/Exam2'
+
 function App() {
-  return <Parent />
+  return <Exam2 />
 }
 
 export default App
