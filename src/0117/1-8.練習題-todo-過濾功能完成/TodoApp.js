0e6b2dfa4033dd41bf27b4c3db9a3dfdc275d000
Jin22221 <117150611+Jin22221@users.noreply.github.com>
2023-07-20 00:08:47 +0800

    filter function finished

---
+++ src/0117/1-8.練習題-todo-過濾功能完成/TodoApp.js
@@ -0,0 +1,102 @@
+import { useState } from 'react'
+import './TodoApp.scss'
+import AddForm1 from './components/AddForm1'
+import TodoList from './components/TodoList'
+
+function TodoApp() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: '買牛奶', completed: false },
+    { id: 2, text: '學react', completed: true },
+  ])
+
+  const [filter, setFilter] = useState('All')
+  const filterOptions = ['All', 'Active', 'Completed']
+  const filterOptionsChinese = ['所有', '進行中', '已完成']
+
+  const getFilterTodos = (todos) => {
+    // if (filter === 'All') return todos
+
+    // if (filter === 'Active')
+    //   return todos.filter((v, i) => {
+    //     return v.completed === false
+    //   })
+
+    // if (filter === 'Completed')
+    //   return todos.filter((v, i) => {
+    //     return v.completed === true
+    //   })
+    switch (filter) {
+      case 'Active':
+        return todos.filter((v, i) => {
+          return !v.completed
+        })
+      case 'Completed':
+        return todos.filter((v, i) => {
+          return v.completed
+        })
+      case 'All':
+      default:
+        return todos
+    }
+  }
+
+  const addTodo = (text) => {
+    const newTodo = {
+      id: Number(new Date()),
+      text: text,
+      completed: false,
+    }
+
+    setTodos([newTodo, ...todos])
+  }
+
+  const toggleCompleted = (id) => {
+    const newTodos = todos.map((v) => {
+      if (id === v.id) return { ...v, completed: !v.completed }
+      return { ...v }
+    })
+    setTodos(newTodos)
+  }
+
+  const deleteTodo = (id) => {
+    const newTodos = todos.filter((v, i) => {
+      // console.log(`v.id= ${v.id}`, `id= ${id}`)
+      return v.id !== id
+    })
+
+    setTodos(newTodos)
+  }
+
+  return (
+    <>
+      <div className="container">
+        <h1>待辦事項</h1>
+        <hr />
+        <AddForm1 addTodo={addTodo} />
+        <TodoList
+          todos={getFilterTodos(todos)}
+          toggleCompleted={toggleCompleted}
+          deleteTodo={deleteTodo}
+        />
+        <hr />
+        {filterOptions.map((v, i) => {
+          return (
+            <button
+              key={i}
+              className={
+                filter === v ? 'filter-btn-active' : 'filter-btn-normal'
+              }
+              onClick={() => {
+                setFilter(v)
+              }}
+            >
+              {filterOptionsChinese[i]}({v})
+            </button>
+          )
+        })}
+      </div>
+    </>
+  )
+}
+
+export default TodoApp
--- src/App.js
+++ src/App.js
@@ -24,8 +24,9 @@
 // import TodoApp from './0117/1-1拆分元件/TodoApp'
 // import TodoApp from './0117/1-5拆分元件 -資料夾對應階層/TodoApp'
 // import TodoApp from './0117/1-6.練習題-todo-過濾項目操作介面與state/TodoApp'
+// import TodoApp from './0117/1-7.練習題-todo-過濾項目按鈕改為map與加入中文/TodoApp'
 
-import TodoApp from './0117/1-7.練習題-todo-過濾項目按鈕改為map與加入中文/TodoApp'
+import TodoApp from './0117/1-8.練習題-todo-過濾功能完成/TodoApp'
 
 function App() {
   return (
